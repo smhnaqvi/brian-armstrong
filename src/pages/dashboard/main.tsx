f78ee665a949084c01702e0136174c36b2d9779f
smhnaqvi <smhnaqvi111@gmail.com>
2026-01-03 21:30:08 +0500

    Refactor dashboard main component to remove unused state management
    - Removed unnecessary useState import from the main dashboard file to streamline the code and improve readability.

--- src/pages/dashboard/main.tsx
+++ src/pages/dashboard/main.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import PriceItemBox from "../../components/priceItem"
 import CalenderTimeline from "../../components/calenderTimeline"
 
